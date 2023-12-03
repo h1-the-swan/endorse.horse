462a28276ae1fe714fe773506353d26d460faec8
h1-the-swan <jason.portenoy@gmail.com>
2023-12-03 12:36:19 -0500

    initial commit

---
+++ components/EndorseHorse.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ props.service_or_item }}</h1>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  episode_date: string;
+  service_or_item: string;
+  endorser: string;
+  category: string;
+  notes?: string;
+}>()
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <div class="content" v-if="endorseHorses">
+      <EndorseHorse v-for="endorse in endorseHorses[0].body" v-bind="(endorse as any)"></EndorseHorse>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { data: endorseHorses } = await useAsyncData('endorseHorses', () => queryContent('/endorse_test').find())
+</script>
+
+<style lang="scss">
+$break-small: 765px;
+
+body {
+  margin: 0;
+  overflow: scroll;
+  overflow-x: hidden;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+#app {
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0px 40px;
+  font-family: sans-serif;
+  text-align: center;
+  font-size: 16px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  @media screen and (max-width: $break-small) {
+    padding: 0 5%;
+  }
+}
+
+.content {
+  flex-grow: 1;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-family: 'Raleway', sans-serif;
+}
+
+h3 {
+  letter-spacing: .06em;
+}
+
+.footer-bar {
+  background-image: url('./assets/baby.png');
+  height: 48px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 100%;
+  background-position: center;
+  margin-top: 100px;
+  transform: translateY(100%);
+  transition: transform 0.5s;
+  overflow: hidden;
+
+  &.goodBabyShowing {
+    transform: translateY(0%);
+  }
+}</style>
